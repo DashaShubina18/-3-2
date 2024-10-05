6d66a44187087d0273f61acf62e2e06a3ea7e35d
Даша Шубіна <shubinad268@gmail.com>
2024-10-05 18:30:04 +0300

    Add project files.

---
+++ лаба 3 завдання 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба_3_завдання_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1.25;
+            double e = Math.Pow(10, -6);
+            double sum = 0;
+            double term;
+            double fault = 1;
+            int n = 1;
+            Console.OutputEncoding=Encoding.UTF8;
+            do
+            {
+                term = Math.Pow(n, (Math.Log(n)) / (Math.Pow((x + Math.Log(n)), n)));
+                sum += term;
+                fault = Math.Abs(term/sum);
+                n++;
+            } while (fault>e);
+            Console.WriteLine($"Номер ітерації = {n}");
+            Console.WriteLine($"Величина поточного ряду: {term}");
+            Console.WriteLine($"Накопичена сума S2: {sum} ");
+            Console.WriteLine($"Досягнута на поточній ітерації похибка: {fault}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
